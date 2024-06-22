81cc5918bc8b9a9b232b2f7208be9daed3b65bea
Eric Dai <ericdai123@gmail.com>
2024-06-22 01:24:47 -0500

    Added a button to prompt the user to pick the dimensions for the grid, and change the size accordingly.]

--- script.js
+++ script.js
@@ -1,40 +1,96 @@
-const GRID_SIZE = 16;
+// Global pre-defined constants and variables
+const BORDER_WIDTH = 32;
+const BUTTON_HEIGHT = 80;
 
-function setUpContainer() {
-  const body = document.querySelector('body');
-  
+let gridWidth = window.innerWidth;
+let gridHeight = window.innerHeight;
+let gridDimensions = 16;
+
+const body = document.querySelector('body');
+
+// Adds a button that prompts the user to enter the # of squares they want for each side of the new grid
+function setUpNewGridButton() {
+  let newGridButton = document.createElement('button');
+  newGridButton.textContent = 'Create New Grid';
+  body.appendChild(newGridButton);
+
+  newGridButton.addEventListener('click', () => {
+    try {
+      gridDimensions = prompt('Enter the # of squares would you like on each side of the new Grid:');
+
+      if (gridDimensions == null || isNaN(gridDimensions) || gridDimensions > 100 || gridDimensions < 1) {
+        throw new Error("Not a valid number, or dimensions are too large.");
+      }
+      
+      resetGrid();
+      createGrid();
+    }
+    catch (error) {
+      alert('Invalid input. Please enter a valid number.')
+    }
+  })
+}
+
+function setUpContainer() {  
   let gridContainer = document.createElement('div');
   gridContainer.id = 'container';
-
   body.appendChild(gridContainer);
 }
 
-// Main function to add a div into the html body
-function createGrid() {
-  try {
-    const gridContainer = document.querySelector('#container');
+// Empty out the grid due to a new one being requested, and set the correct dimensions for each grid box
+function resetGrid() {
+  const gridContainer = document.querySelector('#container');
 
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(container.firstChild);
+  }
+}
+
+function createGrid() {
+  const gridContainer = document.querySelector('#container');
+
+  for (let i = 0; i < gridDimensions; i++) {
+    for (let j = 0; j < gridDimensions; j++) {
+      addBox(gridContainer);
+    }
+  }
+
+  setGridSize();
+}
+
+// Main function to add a certain sized div into the grid
+function addBox(gridContainer) {
+  try {
     let newDiv = document.createElement('div');
-    newDiv.classList.add('grid');
+    newDiv.classList.add('grid-box');
   
     gridContainer.appendChild(newDiv);  
   }
   catch (err) {
-    console.log(`Original grid not created, error: ${err}`);
+    alert(`Original grid not created, error: ${err}`);
   }
 }
 
+function setGridSize() {
+  const gridBoxes = document.querySelectorAll('.grid-box');
+
+  gridWidth = window.innerWidth;
+  gridHeight = window.innerHeight;
+  
+  gridBoxes.forEach((gridBox) => {
+    gridBox.style.width = `${(gridWidth - BORDER_WIDTH) / gridDimensions}px`;
+
+    gridBox.style.height = `${(gridHeight - BUTTON_HEIGHT) / gridDimensions}px`;
+  });
+}
+
+// Main meat of the project comes from the following three functions, with inner helper functions in the correct order.
+setUpNewGridButton();
 setUpContainer();
-
-// Call the createGrid function 16 x 16 times
-for (let i = 0; i < GRID_SIZE; i++) {
-  for (let j = 0; j < GRID_SIZE; j++) {
-    createGrid();
-  }
-}
+createGrid();
 
 // Handle hover event for each box of the 16x16 grid
-const gridBoxes = document.querySelectorAll('.grid');
+const gridBoxes = document.querySelectorAll('.grid-box');
 gridBoxes.forEach((gridBox) => {
   gridBox.addEventListener('mouseenter', () => {
     gridBox.classList.add('hover');
